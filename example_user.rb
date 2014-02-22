4c27e28e38346b0802b5a8755ded04d2c998b5fb
Tony Huang <ttonyhh@gmail.com>
2014-02-22 16:51:05 -0700

    Finish layout and routes

---
+++ example_user.rb
@@ -0,0 +1,16 @@
+class User
+  attr_accessor :name, :email
+
+  def initialize(attributes = {})
+    @name  = attributes[:name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
+
+  def email_first
+  	"#{@email} <#{@name}>"
+  end
+end
